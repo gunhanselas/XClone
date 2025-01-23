de1e27ff5261ce523002d4226b7f404360337236
Stephan Dowless <slcd09@yahoo.com>
2025-01-23 12:12:12 -0500

    [Feature] Project Setup & SwiftLint Install

---
+++ XClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  XClone
+//
+//  Created by Stephan Dowless on 1/23/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
