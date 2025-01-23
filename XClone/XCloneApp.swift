afd517e756a149ecf134c7ccf21397edaeacf8d2
Stephan Dowless <slcd09@yahoo.com>
2025-01-23 10:24:49 -0500

    Initial Commit

---
+++ XClone/XCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCloneApp.swift
+//  XClone
+//
+//  Created by Stephan Dowless on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct XCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCloneTests/XCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  XCloneTests.swift
+//  XCloneTests
+//
+//  Created by Stephan Dowless on 1/23/25.
+//
+
+import Testing
+@testable import XClone
+
+struct XCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
