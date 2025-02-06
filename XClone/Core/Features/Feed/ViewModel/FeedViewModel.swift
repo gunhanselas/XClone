d9aeae5c76d85ebe0558d39e6e95b0e7710dcc93
Stephan Dowless <slcd09@yahoo.com>
2025-02-06 15:51:17 -0500

    [Feature][Feed] Clean up code in view model

--- XClone/Core/Features/Feed/ViewModel/FeedViewModel.swift
+++ XClone/Core/Features/Feed/ViewModel/FeedViewModel.swift
@@ -32,34 +32,4 @@ class FeedViewModel: FeedViewModelProtocol {
             loadingState = .error(error)
         }
     }
-    
-    func likePost(_ post: Post) async {
-        guard let index = posts.firstIndex(where: { $0.id == post.id }) else { return }
-        
-        do {
-            posts[index].didLike = true
-            posts[index].engagement.likesCount += 1
-            try await likeService.likePost(post)
-        } catch {
-            posts[index].didLike = false
-            posts[index].engagement.likesCount -= 1
-            print("DEBUG: Failed to like post with error: \(error)")
-        }
-    }
-    
-    func unlikePost(_ post: Post) async {
-        guard post.didLike,
-              post.engagement.likesCount > 0,
-              let index = posts.firstIndex(where: { $0.id == post.id }) else { return }
-        
-        do {
-            posts[index].didLike = false
-            posts[index].engagement.likesCount -= 1
-            try await likeService.unlikePost(post)
-        } catch {
-            posts[index].didLike = true
-            posts[index].engagement.likesCount += 1
-            print("DEBUG: Failed to unlike post with error: \(error)")
-        }
-    }
 }
