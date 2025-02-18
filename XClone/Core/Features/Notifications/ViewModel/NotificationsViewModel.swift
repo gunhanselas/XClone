696514d64bcde55f1767c19dd10fb73c6a841178
Stephan Dowless <slcd09@yahoo.com>
2025-02-18 17:34:34 -0500

    [Feature][Notifications] Fetch notif user data

--- XClone/Core/Features/Notifications/ViewModel/NotificationsViewModel.swift
+++ XClone/Core/Features/Notifications/ViewModel/NotificationsViewModel.swift
@@ -13,18 +13,41 @@ class NotificationsViewModel {
     var notifications = [XNotification]()
     
     private let service: XNotificationServiceProtocol
+    private let userService: UserServiceProtocol
     
-    init(service: XNotificationServiceProtocol = MockXNotificationService()) {
+    init(
+        service: XNotificationServiceProtocol = XNotificationService(),
+        userService: UserServiceProtocol = UserService()
+    ) {
         self.service = service
+        self.userService = userService
     }
     
     func fetchNotifications() async {
         do {
             notifications = try await service.fetchNotifications()
+            try await fetchNotificationUserData()
             loadingState = notifications.isEmpty ? .empty : .complete
         } catch {
             loadingState = .error(error)
             print("DEBUG: Failed to fetch notifications with error: \(error)")
+        }
+    }
+    
+    private func fetchNotificationUserData() async throws {
+        try await withThrowingTaskGroup(of: (Int, User?).self) { [weak self] group in
+            guard let self else { return }
+            
+            for (index, notification) in self.notifications.enumerated() {
+                group.addTask {
+                    let user = try await self.userService.fetchUser(withUid: notification.senderID)
+                    return (index, user)
+                }
+            }
+            
+            for try await (index, user) in group {
+                notifications[index].sender = user
+            }
         }
     }
 }
